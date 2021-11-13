d02d2dec8feac44f1f5e0bb44e16d194b346b888
Ross <ross@eml.cc>
2021-11-13 12:37:20 -0800

    Make enemies only detect player if on same inversion layer

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -8,9 +8,11 @@ var item_count: int = 0
 var velocity: Vector2 = Vector2()
 var safe = false
 
+
 func _ready():
 	pass
-	
+
+
 func get_input():
 	velocity = Vector2()
 	if Input.is_action_pressed("right"):
@@ -24,15 +26,27 @@ func get_input():
 	velocity = velocity.normalized() * speed
 	
 	if Input.is_action_just_pressed("invert"):
-		g.inverted = !g.inverted
-		if g.inverted:
-			$ColorRect.show()
-		else:
-			$ColorRect.hide()
+		toggle_inversion()
+
 
 func _physics_process(delta):
 	get_input()
 	velocity = move_and_slide(velocity)
+
+
+func toggle_inversion():
+	g.inverted = !g.inverted
+	if g.inverted:
+		set_collision_layer_bit(3, false)
+		set_collision_layer_bit(4, true)
+		$ColorRect.show()
+	else:
+		set_collision_layer_bit(3, true)
+		set_collision_layer_bit(4, false)
+		$ColorRect.hide()
+	print(get_collision_layer_bit(3))
+	print(get_collision_layer_bit(4))
+
 
 func _on_PickupArea_area_shape_entered(area_rid, area, area_shape_index, local_shape_index):
 	if area.name == 'ItemArea':
@@ -41,6 +55,7 @@ func _on_PickupArea_area_shape_entered(area_rid, area, area_shape_index, local_s
 		area.get_parent().queue_free()
 	if area.name == 'SafeZoneArea':
 		safe = true
+
 
 func _on_PickupArea_area_shape_exited(area_rid, area, area_shape_index, local_shape_index):
 	if area and area.name == 'SafeZoneArea':
