bb28b85042401a9e6946b60cb4f464ef89a53740
Bryan <bryan87reed@gmail.com>
2021-11-12 22:03:57 -0700

    Baby's first patrol

--- Scripts/Enemy.gd
+++ Scripts/Enemy.gd
@@ -1,16 +1,47 @@
 extends KinematicBody2D
 
+export (String, "loop", "linear") var patrol_type = "linear"
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+onready var pathfollow = get_parent()
 
+var speed: int = 70
+var run_speed: int = 90
+var state: String = 'patrol'
+var last_patrol_pos: Vector2 = position
+var velocity = Vector2.ZERO
+var player: KinematicBody2D = null
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+	if state == 'patrol':
+		patrol(delta)
+	if state == 'chase':
+		if player:
+			velocity = global_position.direction_to(player.global_position) * run_speed
+			velocity = move_and_slide(velocity)
+	if state == 'return':
+		velocity = global_position.direction_to(last_patrol_pos) * run_speed
+		velocity = move_and_slide(velocity)
+		if round_pos(global_position) == round_pos(last_patrol_pos):
+			state = 'patrol'
+
+func patrol(delta):
+	if patrol_type == 'loop':
+		pathfollow.offset += speed * delta
+		last_patrol_pos = global_position
+
+func _on_DetectionArea_body_entered(body):
+	if body.name == 'Player':
+		player = body
+		state = 'chase'
+
+func _on_DetectionArea_body_exited(body):
+	if body.name == 'Player':
+		player = null
+		state = 'return'
+
+func round_pos(pos: Vector2) -> Vector2:
+	return Vector2(stepify(pos.x, 10), stepify(pos.y, 10))
