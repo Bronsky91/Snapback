12e364e5247004167d7308281f4705fc67a132e9
Bryan <bryan87reed@gmail.com>
2021-11-14 22:50:34 -0700

    Finish tutorial

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -5,10 +5,10 @@ export (int) var sneak_speed: int = 100
 
 onready var anim_player: AnimationPlayer = $AnimationPlayer
 
-onready var player_start_node: Position2D = get_parent().get_node("PlayerStart")
-onready var life_count_label: Label = get_parent().get_node("UI/LifeCountLabel")
-onready var slices_count_label: Label = get_parent().get_node("UI/SlicesCountLabel")
-onready var slices_icon: TextureRect = get_parent().get_node("UI/PizzaSlices")
+onready var player_start_node: Position2D = get_node("/root/Game/PlayerStart")
+onready var life_count_label: Label = get_node("/root/Game/UI/LifeCountLabel")
+onready var slices_count_label: Label = get_node("/root/Game/UI/SlicesCountLabel")
+onready var slices_icon: TextureRect = get_node("/root/Game/UI/PizzaSlices")
 
 var slices_count: int = 4
 var speed: int = run_speed
