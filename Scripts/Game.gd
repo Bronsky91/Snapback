d4d3f890705f8bea06ade4312121904dd16d4043
Bryan <bryan87reed@gmail.com>
2021-11-15 23:57:38 -0700

    Changes timer to red on negative

--- Scripts/Game.gd
+++ Scripts/Game.gd
@@ -1,30 +1,33 @@
 extends Node
 
 onready var pizza_timer: Timer = get_node("PizzaTimer")
-onready var pizza_timer_label: Label = get_node("UI/PizzaTimerLabel")
+onready var pizza_timer_label: RichTextLabel = get_node("UI/PizzaTimerLabel")
 
 var pizza_time = 1800 # 1800 seconds in 30 min
 
 func _ready():
 	pizza_timer.start()
-	pizza_timer_label.text = "30:00"
+	pizza_timer_label.bbcode_text = "30:00"
 
 
 #func _process(delta):
 #	pass
 
 func format_time():
-	return str(ceil(pizza_time/60)).pad_zeros(2) + ":" + str("%01d" % ceil(pizza_time % 60)).pad_zeros(2)
+	return str(abs(ceil(pizza_time/60))).pad_zeros(2) + ":" + str("%01d" % abs(ceil(pizza_time % 60))).pad_zeros(2)
 	
 # .5 seconds is 1 second in game
-func _on_PizzaTimer_timeout():
-	pizza_time -= 1 
-	pizza_timer_label.text = format_time()
+func _on_PizzaTimer_timeout():		
+	pizza_time -= 1
+	if pizza_time < 0:
+		pizza_timer_label.bbcode_text = '[color=red] - ' + format_time()
+	else:
+		pizza_timer_label.bbcode_text = format_time()
 
 func reset_pizza_time():
 	pizza_time = 1800
-	pizza_timer_label.text = "30:00"
+	pizza_timer_label.bbcode_text = "30:00"
 
 func add_time():
 	pizza_time += 120
-	pizza_timer_label.text = format_time()
+	pizza_timer_label.bbcode_text = format_time()
