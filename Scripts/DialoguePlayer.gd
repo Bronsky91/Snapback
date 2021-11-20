e9680ecf413b63a1e5591f0d290497932803716d
Ross <ross@eml.cc>
2021-11-19 21:40:17 -0800

    Scaffold lich dialogue player

--- Scripts/DialoguePlayer.gd
+++ Scripts/DialoguePlayer.gd
@@ -10,7 +10,7 @@ func _ready():
 	dialogues = load_dialogue(skele_dialogue_file)
 	in_danger_dialogues = load_dialogue(in_danger_skele_dialogue_file)
 
-	
+
 func skele_play(status, skelly_name, skelly_text):
 	randomize()
 	var dialogues_array = concat_arrays(dialogues, in_danger_dialogues) if status == 'in danger' else dialogues
@@ -19,12 +19,20 @@ func skele_play(status, skelly_name, skelly_text):
 	$Name.bbcode_text = skelly_name
 	$Message.bbcode_text = skelly_text if skelly_text else random_dialogue['text']
 	show()
-	
+
+
 func load_dialogue(dialogue_file):
 	var file = File.new()
 	if file.file_exists(dialogue_file):
 		file.open(dialogue_file, file.READ)
 		return parse_json(file.get_as_text())
+
+
+func lich():
+	$Name.bbcode_text = "???"
+	$Message.bbcode_text = "You arrive at last..."
+	self.visible = true
+
 
 func concat_arrays(arr1, arr2):
 	arr1 += arr2
