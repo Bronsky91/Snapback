f0426db546db1105c94e064113e7c63944181092
Ross <ross@eml.cc>
2021-11-19 21:10:00 -0800

    Add lich to boss room animation sequence

--- Scripts/BossRoom.gd
+++ Scripts/BossRoom.gd
@@ -2,3 +2,9 @@ extends Node2D
 
 func _ready():
 	$SceneAnimator.play("start")
+
+
+func _grumble():
+	$SFX.stream = load("res://Assets/Audio/lich_stomach_growl.mp3")
+	$SFX.play()
+	g.emit_signal('shake', 3, 18, 18, 0)
