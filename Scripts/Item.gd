4b24edd63f43885c84eba0adca20c885c2349545
Bryan <bryan87reed@gmail.com>
2021-11-19 23:58:50 -0700

    Help me

--- Scripts/Item.gd
+++ Scripts/Item.gd
@@ -1,10 +1,26 @@
 extends Node2D
 
 
-export(String, 'Watch', 'Coin') var type = 'Watch'
+export(String, 'Watch', 'Coin', 'Inverted_Coin') var type = 'Coin'
 
 
 func _ready():
+	g.connect("invert", self, "_on_Player_invert")
+	
 	$Sprite.texture = load('res://Assets/' + type + '.png')
+	
+	if type == "Inverted_Coin":
+		$Sprite.modulate.a = 0.2
 
-
+func _on_Player_invert(inverted):
+	if type == "Inverted_Coin":
+		if inverted:
+			$Sprite.modulate.a = 1
+		else:
+			$Sprite.modulate.a = 0.15
+	else:
+		if inverted:
+			$Sprite.modulate.a = 0.15
+		else:
+			$Sprite.modulate.a = 1
+	
