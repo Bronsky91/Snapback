4adcd77369424ddc02b8702f3785c2e97a6b7fb5
Ross <ross@eml.cc>
2021-11-19 22:44:26 -0800

    Add inverted lich to boss room

--- Scripts/BossRoom.gd
+++ Scripts/BossRoom.gd
@@ -1,5 +1,7 @@
 extends Node2D
 
+var can_invert = false
+
 func _ready():
 	$SceneAnimator.play("start")
 
@@ -8,3 +10,25 @@ func _grumble():
 	$SFX.stream = load("res://Assets/Audio/lich_stomach_growl.mp3")
 	$SFX.play()
 	g.emit_signal('shake', 3, 18, 18, 0)
+
+
+func _space():
+	$UI/Space.visible = true
+	can_invert = true
+
+
+func _input(event):
+	if event.is_action_pressed("invert") and can_invert:
+		can_invert = false
+		invert()
+
+
+func invert():
+	$UI/Space.visible = false
+	$UI/Shockwave/AnimationPlayer.play("invert")
+	$UI/Shockwave.visible = true
+	$YSort/PlayerOnRails/PlayerAnimator.play("InverseIdleBack")
+	$UI/InvertScreen.show()
+	$BlackTop.visible = false
+	$Lich.visible = false
+	$InvertedLich.visible = true
