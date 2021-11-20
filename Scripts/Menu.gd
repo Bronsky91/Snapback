f8c386ffa4a7c31c2ad2b26950e7a4f1f8fba32b
Bryan <bryan87reed@gmail.com>
2021-11-20 13:14:53 -0700

    Music synced to hat flip animation in menu

--- Scripts/Menu.gd
+++ Scripts/Menu.gd
@@ -1,19 +1,12 @@
 extends Node
 
+var seconds = 0
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+var flip_intervals = [10, 41, 51, 72]
 
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+	pass 
 
 
 func _on_StartGame_button_up():
@@ -21,5 +14,19 @@ func _on_StartGame_button_up():
 
 
 func _on_AnimationPlayer_animation_finished(anim_name):
-	if anim_name == "Invert":
-		$InvertScreen.visible = true
+	if anim_name == "Inverse":
+		$Pizzaboy/AnimationPlayer.play("IdleReverse")
+	else:
+		$InvertScreen.visible = false
+		$Pizzaboy/AnimationPlayer.play("Idle")
+
+
+func _on_Timer_timeout():
+	seconds = seconds + 1
+	if flip_intervals.has(seconds):
+		flip_it()
+
+func flip_it():
+	var animation_name = 'Inverse' if not $InvertScreen.visible else 'Reverse'
+	$Pizzaboy/AnimationPlayer.play(animation_name)
+	$InvertScreen.visible = animation_name == 'Inverse'
