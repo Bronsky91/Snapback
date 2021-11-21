50e730908e85c1b0686e377dfae14ccc3a019161
Ross <ross@eml.cc>
2021-11-21 00:03:44 -0800

    Calculate final score

--- Scripts/ReceiptScore.gd
+++ Scripts/ReceiptScore.gd
@@ -7,3 +7,8 @@ func _ready():
 	$Time.text = g.final_score.timestamp
 	$Skelebois.text = str(g.final_score.skellies_talked_to) + " pts"
 	$Inverted.text = str(g.final_score.inversions) + " times"
+	var score_num = g.final_score.time
+	score_num += g.final_score.coins * 5
+	score_num += g.final_score.skellies_talked_to * 20
+	score_num += g.final_score.slices_lost * -20
+	$Score.text = str(score_num)
