f2317ba54298281d1f893ced298f83854dc8bf72
Ross <ross@eml.cc>
2021-11-21 10:57:23 -0800

    Remove assert wrapper around change scene call

--- Scripts/SceneChanger.gd
+++ Scripts/SceneChanger.gd
@@ -9,6 +9,6 @@ func change_scene(path, delay = 0.5):
 	yield(get_tree().create_timer(delay), "timeout")
 	animation_player.play("fade")
 	yield(animation_player, "animation_finished")
-	assert(get_tree().change_scene(path) == OK)
+	get_tree().change_scene(path)
 	animation_player.play_backwards("fade")
 	emit_signal("scene_changed")
