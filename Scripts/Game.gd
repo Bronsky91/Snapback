30e257368adb55ec5eb783dcab0cfd4241f0d64b
Bryan <bryan87reed@gmail.com>
2021-11-21 10:24:03 -0700

    Updates button for fullscreen

--- Scripts/Game.gd
+++ Scripts/Game.gd
@@ -16,6 +16,11 @@ func _ready():
 	pizza_timer.start()
 	pizza_timer_label.bbcode_text = "30:00"
 	coin_count_label.bbcode_text = str(coin_count)
+	
+func _input(event):
+	if event.is_action_pressed("ui_cancel") and OS.window_fullscreen:
+		OS.window_fullscreen = false
+		$UI/FullScreenButton.show()
 
 func format_time():
 	return str(abs(ceil(pizza_time/60))).pad_zeros(2) + ":" + str("%01d" % abs(ceil(pizza_time % 60))).pad_zeros(2)
@@ -79,3 +84,4 @@ func _on_ScreenWipePlayer_animation_finished(anim_name):
 
 func _on_Button_pressed():
 	OS.window_fullscreen = !OS.window_fullscreen
+	$UI/FullScreenButton.visible = !OS.window_fullscreen
