fd8e8aaa30c03755818e5809708ce415328fc0c3
Ross <ross@eml.cc>
2021-11-20 21:47:19 -0800

    Receipt tweaks

--- Scripts/ReceiptScore.gd
+++ Scripts/ReceiptScore.gd
@@ -5,8 +5,8 @@ func _ready():
 	$Coins.bbcode_text = right(g.final_score.coins)
 	$Slices.bbcode_text = right(g.final_score.slices_lost)
 	$Time.bbcode_text = right(g.final_score.timestamp)
-	$Skelebois.bbcode_text = right(g.final_score.skellies_talked_to)
-	$Inverted.bbcode_text = right(g.final_score.inversions)
+	$Skelebois.bbcode_text = right(str(g.final_score.skellies_talked_to) + " pts")
+	$Inverted.bbcode_text = right(str(g.final_score.inversions) + " times")
 
 
 # Called when the node enters the scene tree for the first time.
