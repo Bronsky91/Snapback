bbdc1a35f9d45ce5659aa9c2c4e42ad57599f33b
Bryan <bryan87reed@gmail.com>
2021-11-20 21:43:34 -0700

    Menu is up to MEEmao's standards

--- Scripts/Menu.gd
+++ Scripts/Menu.gd
@@ -1,12 +1,16 @@
 extends Node
 
-var seconds = 0
+var seconds = 0.0
 
-var flip_intervals = [10, 41, 51, 72]
-
+var flip_intervals = [10.00, 40.50, 51.00, 71.50]
+var reverse_intervals = [41.00, 72.00]
 
 func _ready():
 	pass 
+	
+func _input(event):
+	if event.is_action_pressed('ui_accept'):
+		get_tree().change_scene("res://Scenes/Game.tscn")
 
 
 func _on_StartGame_button_up():
@@ -17,21 +21,28 @@ func _on_AnimationPlayer_animation_finished(anim_name):
 	if anim_name == "Inverse":
 		$Pizzaboy/AnimationPlayer.play("IdleReverse")
 	else:
-		$CanvasLayer/InvertScreen.visible = false
 		$Pizzaboy/AnimationPlayer.play("Idle")
 
 
 func _on_Timer_timeout():
-	seconds = seconds + 1
+	seconds = seconds + 0.5
+	print(seconds)
 	if flip_intervals.has(seconds):
 		flip_it()
+	if reverse_intervals.has(seconds):
+		$CanvasLayer/InvertScreen.visible = false
+	if seconds == 41.00:
+		$Pizzaboy/LichEyes/AnimationPlayer.play("LichEyeBounce")
+	if seconds ==  77.0:
+		seconds = 0
 
 func flip_it():
 	var animation_name = 'Inverse' if not $CanvasLayer/InvertScreen.visible else 'Reverse'
 	$Pizzaboy/AnimationPlayer.play(animation_name)
-	$CanvasLayer/InvertScreen.visible = animation_name == 'Inverse'
+	if animation_name == 'Inverse':
+		$CanvasLayer/InvertScreen.visible = true
 
 
 func _on_StartGame_pressed():
-	print('hi')
 	get_tree().change_scene("res://Scenes/Game.tscn")
+
